2b927ea33da8b715980b56377fb38dc0c28e4c34
Joseph Delong <joseph@delong.me>
2022-02-07 23:50:27 -0600

    implement signing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/ethereum-provider";
-import { Contract, providers, utils } from "ethers";
+import { Contract, providers, utils, BigNumber } from "ethers";
 
 // @ts-ignore
 import logo from "./logo.svg";
 import "./App.css";
 import { formatAuthMessage } from "./utils";
-import { DAI } from "./constants";
+import { DAI, LSDHELPER, LSD, BADTRIP } from "./constants";
 
 function App() {
   const web3Modal = new Web3Modal({
@@ -17,7 +17,7 @@ function App() {
       walletconnect: {
         package: WalletConnectProvider,
         options: {
-          infuraId: process.env.REACT_APP_INFURA_ID,
+          // infuraId: process.env.REACT_APP_INFURA_ID,
         },
       },
     },
@@ -69,6 +69,91 @@ function App() {
     console.log("res", res);
   }
 
+  async function redeemLSD() {
+    if (!provider) {
+      throw new Error("Provider not connected");
+    }
+    const AMOUNT = getBigNumber(1);
+    const signer:providers.JsonRpcSigner = provider.getUncheckedSigner();
+    const helper = new Contract(LSDHELPER.address, LSDHELPER.abi, signer);
+    const lsd = new Contract(LSD.address, LSD.abi, signer);
+
+    // Signing permit for $LSD token
+    const latest = await provider.getBlockNumber();
+    console.log((await provider.getBlock(latest)).timestamp);
+    console.log("chainId: ", chainId);
+    const deadline = (await provider.getBlock(latest)).timestamp + 10000
+
+    // const wallet = JsonRpcSigner()
+    const signedPermitERC20 = await signPermitERC20(
+        BigNumber.from(chainId),
+        lsd,
+        signer,
+        LSDHELPER.address,
+        AMOUNT,
+        BigNumber.from(deadline)
+    );
+    const erc20Sig = utils.splitSignature(signedPermitERC20)
+    
+    console.log(address, helper.address, AMOUNT, deadline, erc20Sig.v, erc20Sig.r, erc20Sig.s, BigNumber.from(1));
+    // Redeem the $LSD token for NFT
+    await helper.permitAndRedeem(address, helper.address, AMOUNT, deadline, erc20Sig.v, erc20Sig.r, erc20Sig.s, BigNumber.from(1), {
+      gasLimit: 250000
+    });
+  }
+
+  const getBigNumber = (amount:number, decimals = 18) => {
+    const BASE_TEN = 10
+    return BigNumber.from(amount).mul(BigNumber.from(BASE_TEN).pow(decimals))
+  }
+
+  const signPermitERC20 = async (chainId:BigNumber, verifyingContract:Contract, signer:providers.JsonRpcSigner, spender:string, value:BigNumber, deadline:BigNumber) => {
+    const owner = address
+    const nonce = await verifyingContract.nonces(address)
+    
+    const typedData:any = {
+      types: {
+        Permit: [
+          {
+            name: "owner",
+            type: "address"
+          },
+          {
+            name: "spender",
+            type: "address"
+          },
+          {
+            name: "value",
+            type: "uint256"
+          },
+          {
+            name: "nonce",
+            type: "uint256"
+          },
+          {
+            name: "deadline",
+            type: "uint256"
+          }
+        ],
+      },
+      primaryType: 'Permit' as const,
+      domain: {
+        chainId: chainId,
+        verifyingContract: verifyingContract.address,
+      },
+      message: {
+        owner,
+        spender,
+        value,
+        nonce,
+        deadline
+      }
+    }
+    return signer._signTypedData( typedData.domain , typedData.types , typedData.message);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -77,8 +162,9 @@ function App() {
         {address ? (
           <>
             <div>{address}</div>
-            <button onClick={signMessage}>Authenticate</button>
-            <button onClick={transferDai}>Transfer DAI</button>
+            {/* <button onClick={signMessage}>Authenticate</button> */}
+            {/* <button onClick={transferDai}>Transfer DAI</button> */}
+            <button onClick={redeemLSD}>Redeem LSD</button>
           </>
         ) : (
           <button onClick={connect}>Connect</button>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
 export * from "./dai";
+export * from "./lsdhelper";
+export * from "./lsd";
+export * from "./badtrip";
 export * from "./default";
