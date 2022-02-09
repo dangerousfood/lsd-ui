a6d18642f37cce0d183b8a5ca9db46b63eea5001
Joseph Delong <joseph@delong.me>
2022-02-09 11:34:24 -0600

    rough cut redemption of ERC20

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import { Contract, providers, utils, BigNumber } from "ethers";
 // @ts-ignore
 import logo from "./logo.svg";
 import "./App.css";
-import { formatAuthMessage } from "./utils";
-import { DAI, LSDHELPER, LSD, BADTRIP } from "./constants";
+import { LSDHELPER, LSD, BADTRIP } from "./constants";
 
 function App() {
   const web3Modal = new Web3Modal({
@@ -24,18 +23,32 @@ function App() {
   });
 
   const [chainId, setChainId] = React.useState<number>(1);
-  const [address, setAddress] = React.useState<string>("");
+  const [address, setAddress] = React.useState<string|null>(null);
   const [provider, setProvider] = React.useState<providers.Web3Provider>();
   const [allowance, setAllowance] = React.useState<BigNumber>(BigNumber.from(0));
   const [lsdBalance, setLSDBalance] = React.useState<BigNumber>(BigNumber.from(0));
   const [btBalance, setBTBalance] = React.useState<BigNumber>(BigNumber.from(0));
 
+  const [redeemValue, setRedeemVal] = React.useState<string>("");
+
+  React.useEffect(() => {
+    if(address != null && provider != null)getBalances();
+  }, [provider, address]);
+
+  // getBalances()
   function reset() {
     console.log("reset");
     setAddress("");
     setProvider(undefined);
     web3Modal.clearCachedProvider();
   }
+
+
+
+  React.useEffect(() => {
+    console.log("Allowance updated " + allowance);
+    // getBalances();
+  }, [allowance]);
 
   async function connect() {
     if (!process.env.REACT_APP_INFURA_ID) {
@@ -46,6 +59,7 @@ function App() {
     web3Provider.on("disconnect", reset);
 
     const accounts = (await web3Provider.enable()) as string[];
+    const address = accounts[0];
     setAddress(accounts[0]);
     setChainId(web3Provider.chainId);
 
@@ -79,8 +93,12 @@ function App() {
     else {
       const signer:providers.JsonRpcSigner = provider.getSigner();
       const lsd = new Contract(LSD.address, LSD.abi, signer);
+      const balance = await lsd.balanceOf(address);
 
-      await lsd.approve(LSDHELPER.address, getBigNumber(1).toHexString())
+      const transaction = await lsd.approve(LSDHELPER.address, balance.toHexString())
+      transaction.wait().then(function() {
+        getBalances();
+      });
     }
   }
 
@@ -92,7 +110,10 @@ function App() {
       const signer:providers.JsonRpcSigner = provider.getSigner();
       const helper = new Contract(LSDHELPER.address, LSDHELPER.abi, signer);
 
-      await helper.redeem(BigNumber.from(1).toHexString())
+      const transaction = await helper.redeem(BigNumber.from(redeemValue).toHexString())
+      transaction.wait().then(function() {
+        getBalances();
+      });
     }
   }
 
@@ -101,52 +122,12 @@ function App() {
     return BigNumber.from(amount).mul(BigNumber.from(BASE_TEN).pow(decimals))
   }
 
-  const signPermitERC20 = async (chainId:BigNumber, verifyingContract:Contract, signer:providers.JsonRpcSigner, spender:string, value:BigNumber, deadline:BigNumber) => {
-    const owner = address
-    const nonce = await verifyingContract.nonces(address)
-    
-    const typedData:any = {
-      types: {
-        Permit: [
-          {
-            name: "owner",
-            type: "address"
-          },
-          {
-            name: "spender",
-            type: "address"
-          },
-          {
-            name: "value",
-            type: "uint256"
-          },
-          {
-            name: "nonce",
-            type: "uint256"
-          },
-          {
-            name: "deadline",
-            type: "uint256"
-          }
-        ],
-      },
-      primaryType: 'Permit' as const,
-      domain: {
-        chainId: chainId,
-        verifyingContract: verifyingContract.address,
-      },
-      message: {
-        owner,
-        spender,
-        value,
-        nonce,
-        deadline
-      }
-    }
-    return signer._signTypedData( typedData.domain , typedData.types , typedData.message);
+  function handleInputChange(e:HTMLInputElement) {
+    setRedeemVal(e.value.replace(/[^0-9]*/ig, ""));
   }
 
-
+  React.useEffect(() => {
+  }, [redeemValue]);
 
   return (
     <div className="App">
@@ -159,9 +140,14 @@ function App() {
             <div>LSD Balance: {lsdBalance.toString()}</div>
             <div>Allowance: {allowance.toString()}</div>
             <div>BadTrip Balance: {btBalance.toString()}</div>
-            <button onClick={getBalances}>Refresh</button>
-            <button onClick={setApprove}>Approve</button>
-            <button onClick={redeem}>Redeem</button>
+            {allowance.gte(getBigNumber(1)) ? 
+              <>
+                <input type="text" pattern="[0-9]*" onChange={e => handleInputChange(e.target as HTMLInputElement)} value={redeemValue}/>
+                <button onClick={redeem}>Redeem</button>
+              </> : 
+              <button onClick={setApprove}>Approve</button>
+            }
+            
           </>
         ) : (
           <button onClick={connect}>Connect</button>
